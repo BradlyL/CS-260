7836da6811ea2318c4ac5a617f4c5ca11194888e
Bradly landucci <bradly.landucci.5149@mail.linnbenton.edu>
2021-02-10 12:09:03 -0800

    Structure

---
+++ Assignment_6/Binary_Tree_Driver.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#include "tree.h"
+
+int main(){
+
+
+
+
+
+    return 0;
+}
---
+++ Assignment_6/Tree.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "node.h"
+using namespace std;
+
+//test_add
+
+//test_remove
+
+//test_traverse
+
+//Add function
+
+//Remove function
+
+//Possible traversal function
