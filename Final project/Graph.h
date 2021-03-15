23b480bd7fc6b95a3da43bdb1006ac13e65ce347
BradlyL <bradly.landucci.5149@mail.linnbenton.edu>
2021-03-14 19:49:36 -0700

    Updated

--- Final project/Graph.h
+++ Final project/Graph.h
@@ -1,6 +1,3 @@
-// A C++ program for Dijkstra's single source shortest path algorithm. 
-// The program is for adjacency matrix representation of the graph 
-
 #include <limits.h> 
 #include <stdio.h>
 #include <iostream>
@@ -8,10 +5,38 @@
 using namespace std;
 
 //Adjusts vertices in graph
-#define V 9 
+#define V 3 
 
-// This functions helps us find the minimum distance out of current distances not visited
-int minDistance(int dist[], bool visited[]) 
+class graph
+{
+private:
+	int mindist(int dist[], bool visited[]);
+
+public:
+	int mat[V][V];
+	void create_matrix();
+	void add_edge(int source, int dest, int weight);
+	void shortest_path(int graph[V][V], int src);
+	void mst();
+	void printer();
+};
+//Creates blank matrix so that edges can be included
+void graph::create_matrix() {
+	for (int i = 0; i < V; i++){
+		for (int j = 0; j < V; j++) {
+			mat[i][j] = 0;
+		}
+	}
+}
+
+void graph::add_edge(int source, int dest, int weight) {
+	mat[source][dest] = weight;
+    mat[dest][source] = weight;
+
+}
+
+//Finds min value out of unvisited vertices
+int graph::mindist(int dist[], bool visited[]) 
 { 
 	//Set min value for evaluation of dist array and bool array
 	int min = INT_MAX, min_index; 
@@ -19,45 +44,37 @@ int minDistance(int dist[], bool visited[])
 	for (int v = 0; v < V; v++) {
 		if (visited[v] == false && dist[v] <= min) {
 			min = dist[v], min_index = v;
-			cout << "min: " << min << endl;
-			cout << "distv: " << dist[v] <<endl;
 		}
 	}
-	//cout << min_index; // Test
 	return min_index; 
 } 
 
 //Finds shortest path to all nodes in graph.
-void shortest_path(int graph[V][V], int src) 
+void graph::shortest_path(int graph[V][V], int src) 
 { 
-	int dist[V]; // The output array. dist[i] will hold the shortest 
-	// distance from src to i 
+	int dist[V]; //Holds shortest paths from source
 
-	bool visited[V]; // sptSet[i] will be true if vertex i is included in shortest 
-	// path tree or shortest distance from src to i is finalized 
+	bool visited[V]; //Holds visited values
 
 	//Initializes all distances as infinity and sets the visited set to be false as nothing has ran.
-	for (int i = 0; i < V; i++) 
+	for (int i = 0; i < V; i++) {
 		dist[i] = INT_MAX, visited[i] = false; 
-
+	}
 	//Source distance is always 0 as it has no where to travel.
 	dist[src] = 0; 
 
 	for (int count = 0; count < V - 1; count++) { 
 		//Find min distance
-		int min = minDistance(dist, visited); 
-		cout << "U: " << min << endl;
+		int min = mindist(dist, visited); 
 		//Sets index to be true at min value
 		visited[min] = true; 
 
 		for (int v = 0; v < V; v++) {
 
 			//Updates path from u to v by checking conditions, if not part of set (false), and graph[min][v] and the connecting edges don't equal infinity, and the
-			//created edge for that current spot dist(first one is 0) + graph(0,0) < dist[0] = 0, therefore 0 + 0 is not less than 0, so move on to next. First loop of
-			//min finds there is a connection with edges 1/7.
+			//created edge for that current spot dist(first one is 0) + graph(0,0) < dist[0] = 0, therefore 0 + 0 is not less than 0, so move on to next. 
 			if (!visited[v] && (graph[min][v] && dist[min] != INT_MAX) && (dist[min] + graph[min][v] < dist[v])) {
 				dist[v] = dist[min] + graph[min][v]; 
-				cout << "BOb: " << v <<  endl;
 			}
 		}
 	} 
@@ -68,3 +85,15 @@ void shortest_path(int graph[V][V], int src)
 		cout << i << "\t\t" << dist[i] << endl;
 	}
 } 
+
+void graph::printer() {
+	cout << endl;
+	//Printing graph for testing : []
+	for (int i = 0; i < V; i++){
+		cout << endl;
+		for (int j = 0; j < V; j++) {
+			cout << mat[i][j] << "  ";
+		}
+	}
+	cout << endl;
+}
--- Final project/Graph_Driver.cpp
+++ Final project/Graph_Driver.cpp
@@ -1,14 +1,13 @@
 #include <stdio.h>
-#include "graph.h"
+#include "Graph.h"
 
 //Only issue currently is you have to redefine the global variable V within graph.h in order to change the size of the 2d array that defines the graph.
 //I assume there is a better way to do this such as using vectors that can be dynamically allocated, but I ran out of time and resources to try to figure out
 //A better solution. Hopefully this does well enough to demonstrate I understand the concepts! This is the running code, but I also attached some of my other attempts.
-//And here are the links to my resources I used to help me finish this project!
 int main() 
 { 
-	/* Let us create the example graph discussed above */
-	int graph[V][V] = { { 0, 4, 0, 0, 0, 0, 0, 8, 0 }, 
+	graph g;
+	/*int graph1[V][V] = { { 0, 4, 0, 0, 0, 0, 0, 8, 0 }, 
 						{ 4, 0, 8, 0, 0, 0, 0, 11, 0 }, 
 						{ 0, 8, 0, 7, 0, 4, 0, 0, 2 }, 
 						{ 0, 0, 7, 0, 9, 14, 0, 0, 0 }, 
@@ -17,8 +16,86 @@ int main()
 						{ 0, 0, 0, 0, 0, 2, 0, 1, 6 }, 
 						{ 8, 11, 0, 0, 0, 0, 1, 0, 7 }, 
 						{ 0, 0, 2, 0, 0, 0, 6, 7, 0 } }; 
+	*/
 
-	shortest_path(graph, 0); 
+	int graph[V][V];
+
+	bool menu = true;
+	int matrix_creation = 0;
+
+	do {
+		int choice;
+		cout << "Menu:\n1: Create matrix\n2: Add edge\n3: Shortest paths\n4: Minimum spanning tree\n5: Leave menu\n6: Print matrix\nEnter choice: ";
+		cin >> choice;
+		switch (choice)
+		{
+		case 1:
+			if (matrix_creation !=1){
+				g.create_matrix();
+				cout << "Matrix created.\n";
+				memcpy(graph, g.mat, sizeof g.mat);
+				matrix_creation = 1;
+			}
+			else {
+				cout << "Matrix already created.\n";
+			}
+			break;
+		
+		case 2:
+			int source, dest, weight;
+			if (matrix_creation !=1) {
+				cout << "Please create a matrix before attempting to add edges.";
+				break;
+			}
+			else {
+				cout << "Enter source: ";
+				cin >> source;
+				cout << "Enter dest: ";
+				cin >> dest;
+				cout << "Enter weight: ";
+				cin >> weight;
+				g.add_edge(source, dest, weight);
+				cout << "Edge added.";
+				break;
+			}
+
+		case 3:
+			int source_short;
+			if (matrix_creation !=1) {
+				cout << "Please create a matrix before attempting to run shortest path algorithm.";
+				break;
+			}
+			else {
+				cout << "Enter source vertex: ";
+				cin >> source_short;
+				g.shortest_path(graph, source_short);
+				break;
+			}
+		
+		case 4:
+			cout <<"Working: MST"; //g.mst();
+			menu = false;
+			break;
+
+		case 5:
+			cout << "Goodbye";
+			menu = false;
+			break;
+		case 6:
+			if (matrix_creation !=1) {
+				cout << "Please create a matrix before attempting to print it.";
+				break;
+			}
+			else {
+				g.printer();
+				break;
+			}
+
+		default:
+			cout << "No input try again.";
+			break;
+		}
+	} while (menu == true);
 
 	return 0; 
 } 
