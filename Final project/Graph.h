84a07b1c3f40981105b31d80b88cdd4debedcf0a
BradlyL <bradly.landucci.5149@mail.linnbenton.edu>
2021-03-15 11:17:16 -0700

    Finished graph

--- Final project/Graph.h
+++ Final project/Graph.h
@@ -18,7 +18,10 @@ public:
 	void add_edge(int source, int dest, int weight);
 	void shortest_path(int graph[V][V], int src);
 	void mst(int mat_1[V][V]);
-	void printer();
+	void printer(int graph[V][V]);
+
+	//Tests
+	void test_functions(int graph[V][V], int source);
 };
 //Creates blank matrix so that edges can be included
 void graph::create_matrix() {
@@ -29,51 +32,54 @@ void graph::create_matrix() {
 	}
 }
 
+//Adds bidirectional edges at [source][dest] & [dest][source]
 void graph::add_edge(int source, int dest, int weight) {
 	mat[source][dest] = weight;
     mat[dest][source] = weight;
-
 }
 
 //Finds min value out of unvisited vertices
 int graph::mindist_dijkstras(int dist[], bool visited[]) 
 { 
 	//Set min value for evaluation of dist array and bool array
-	int min = INT_MAX, min_index; 
+	int min = INT_MAX;
+	int min_index; 
 
 	for (int v = 0; v < V; v++) {
 		if (visited[v] == false && dist[v] <= min) {
-			min = dist[v], min_index = v;
+			min = dist[v];
+			min_index = v;
 		}
 	}
 	return min_index; 
 } 
 
 //Finds shortest path to all nodes in graph.
-void graph::shortest_path(int graph[V][V], int src) 
+void graph::shortest_path(int graph[V][V], int source) 
 { 
-	int dist[V]; //Holds shortest paths from source
-
-	bool visited[V]; //Holds visited values
+	//Holds shortest paths from source
+	int dist[V];
+	//Holds visited values
+	bool visited[V];
 
 	//Initializes all distances as infinity and sets the visited set to be false as nothing has ran.
 	for (int i = 0; i < V; i++) {
 		dist[i] = INT_MAX, visited[i] = false; 
 	}
 	//Source distance is always 0 as it has no where to travel.
-	dist[src] = 0; 
+	dist[source] = 0; 
 
-	for (int count = 0; count < V - 1; count++) { 
+	for (int i = 0; i < V - 1; i++) { 
 		//Find min distance
 		int min = mindist_dijkstras(dist, visited); 
 		//Sets index to be true at min value
 		visited[min] = true; 
 
-		for (int v = 0; v < V; v++) {
-			//Updates path from u to v by checking conditions, if not part of set (false), and graph[min][v] and the connecting edges don't equal infinity, and the
+		for (int j = 0; j < V; j++) {
+			//Updates path from u to v by checking conditions, if not part of set (false), and graph[min][j] and the connecting edges don't equal infinity, and the
 			//created edge for that current spot dist(first one is 0) + graph(0,0) < dist[0] = 0, therefore 0 + 0 is not less than 0, so move on to next. 
-			if (!visited[v] && (graph[min][v] && dist[min] != INT_MAX) && (dist[min] + graph[min][v] < dist[v])) {
-				dist[v] = dist[min] + graph[min][v]; 
+			if (!visited[j] && (graph[min][j] && dist[min] != INT_MAX) && (dist[min] + graph[min][j] < dist[j])) {
+				dist[j] = dist[min] + graph[min][j]; 
 			}
 		}
 	} 
@@ -86,29 +92,31 @@ void graph::shortest_path(int graph[V][V], int src)
 } 
 
 void graph::mst(int mat_1[V][V]) {
-	int unvisited[V];
+	//Visited array that keeps track of variables that have already been checked for min
 	bool visited[V];
+	//Min tree starts at arbitrary value
 	int source = 0;
+	//These variables I use to control how the information is passed around. Vital part of algorithm as it holds old and new information to keep my if statements/loops in tact
 	int tmp = source;
 	int old_tmp = source;
 	int new_tmp;
+	//Switches from i - tmp after first round so it can go through path.
 	bool first_round = true;
+	//Total weight of min span tree
 	int totalweight = 0;
 
 	for (int i=0;i<V;i++) {
-		unvisited[i] = i;
 		visited[i] = false;
 	}
 
 	visited[source] = true;
-	unvisited[source] = -1;
 
 	for (int i=0;i<V-1;i++){
-		int min = INT_MAX;
-		if (first_round != false) {
+		int min = INT_MAX; //Resets to infinity every iteration of finding next min value
+		if (first_round != false) { //Checks round count
 			for (int j=0; j<V;j++) {
-				if (mat_1[i][j] != 0 && visited[j] != true && unvisited[j] != -1) {
-					if (mat_1[i][j] <= min) {
+				if (mat_1[i][j] != 0 && visited[j] != true) { //Checks if it's not 0 and if it hasn't been visited
+					if (mat_1[i][j] <= min) { //If true and value is less than current min, set min to value and store the column value in tmp var
 						min = mat_1[i][j];
 						new_tmp = j;
 						first_round = false;
@@ -118,8 +126,8 @@ void graph::mst(int mat_1[V][V]) {
 		}
 		else {
 			for (int j=0; j<V;j++) {
-				if (mat_1[tmp][j] != 0 && visited[j] != true && unvisited[j] != -1) {
-					if (mat_1[tmp][j] <= min) {
+				if (mat_1[tmp][j] != 0 && visited[j] != true) {
+					if (mat_1[tmp][j] <= min) { ////If true and value is less than current min, set min to value and store the column value in tmp var
 						min = mat_1[tmp][j];
 						new_tmp = j;
 					}
@@ -127,25 +135,32 @@ void graph::mst(int mat_1[V][V]) {
 
 			}
 		}
-		cout << tmp << "--" << new_tmp << ": " << min << endl;
+		cout << tmp << "--" << new_tmp << ": " << min << endl; //Prints value and connecting vertices
 		visited[tmp] = true;
-		unvisited[tmp] = -1;
-		old_tmp = tmp;
+		old_tmp = tmp; //Rearranging tmp variables to store so for next iteration they aren't replaced
 		tmp = new_tmp;
-		totalweight += min;
+		totalweight += min; //Creating total weight
 	}
 
 	cout << "Total weight of graph is: " << totalweight << endl;
 }
 
-void graph::printer() {
+void graph::printer(int graph[V][V]) {
 	cout << endl;
 	//Printing graph for testing : []
 	for (int i = 0; i < V; i++){
 		cout << endl;
 		for (int j = 0; j < V; j++) {
-			cout << mat[i][j] << "  ";
+			cout << graph[i][j] << "  ";
 		}
 	}
 	cout << endl;
+}
+
+//Testing functions
+void graph::test_functions(int graph[V][V], int source) {
+	create_matrix();
+	shortest_path(graph, source);
+	mst(graph);
+	printer(graph);
 }
--- Final project/Graph_Driver.cpp
+++ Final project/Graph_Driver.cpp
@@ -3,18 +3,18 @@
 
 //Only issue currently is you have to redefine the global variable V within graph.h in order to change the size of the 2d array that defines the graph.
 //I assume there is a better way to do this such as using vectors that can be dynamically allocated, but I ran out of time and resources to try to figure out
-//A better solution. Hopefully this does well enough to demonstrate I understand the concepts! This is the running code, but I also attached some of my other attempts.
+//A better solution. Hopefully this does well enough to demonstrate I understand the concepts!
 int main() 
 { 
 	graph g;
-	/*int graph_1[V][V] = { 
+	int test_graph[V][V] = { 
 						{0,4,0,5,2}
                        ,{4,0,1,3,0}
                        ,{0,1,0,8,0}
 					   ,{5,3,8,0,2}
 					   ,{2,0,0,2,0}
 					   }; 
-	*/ // Testing graph
+	// Testing graph
 	int graph[V][V];
 
 	//Menu / Matrix creation variables
@@ -23,14 +23,14 @@ int main()
 
 	do {
 		int choice;
-		cout << "Menu:\n1: Create matrix\n2: Add edge\n3: Shortest paths\n4: Minimum spanning tree\n5: Print matrix\n6: Leave menu\nEnter choice: ";
+		cout << "Menu:\n1: Create matrix\n2: Add edge\n3: Shortest paths\n4: Minimum spanning tree\n5: Print matrix\n6. Test functions\n7: Leave menu\nEnter choice: ";
 		cin >> choice;
 		switch (choice)
 		{
 		case 1:
 			if (matrix_creation !=1){
 				g.create_matrix();
-				cout << "Matrix created.\n";
+				cout << "Matrix created of size: " << V << "x" << V << endl;
 				memcpy(graph, g.mat, sizeof g.mat);
 				matrix_creation = 1;
 			}
@@ -87,17 +87,20 @@ int main()
 				break;
 			}
 			else {
-				g.printer();
+				g.printer(g.mat);
 				break;
 			}
-
 		case 6:
+			g.test_functions(test_graph, 0);
+			cout << endl << "All functions working properly." << endl;
+			break;
+		case 7:
 			cout << "Goodbye";
 			menu = false;
 			break;
 
 		default:
-			cout << "No input try again.";
+			cout << "False input try again.";
 			break;
 		}
 	} while (menu == true);
